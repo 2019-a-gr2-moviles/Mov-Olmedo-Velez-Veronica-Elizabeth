e90db799972843abf9c00180cfdd1326abd330eb
NikaOlve <veronica_198825@hotmail.com>
2019-05-23 16:07:53 -0500

    Parcelable

---
+++ 03-Android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_parcelable.setOnClickListener {
+            irAParcelable()
+        }
+    }
+
+    fun irAParcelable(){
+        val intentExplicito = Intent(
+            this,
+            Parcelable::class.java
+        )
+        val nika = Usuario("Nika",
+            24,
+            Date(),
+            100.50)
+        intentExplicito.putExtra("usuario",nika)
+        startActivity(intentExplicito)
+
+        val cachetes = Mascotas("Cachetes", nika)
+        intentExplicito.putExtra("mascotas",cachetes)
+        startActivity(intentExplicito)
+    }
+
+
+
+
+}
