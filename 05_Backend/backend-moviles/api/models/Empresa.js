6b822857ec81616345567e85cfedff3f9c95e559
NikaOlve <veronica_198825@hotmail.com>
2019-06-21 16:12:39 -0500

    Servicio

---
+++ 05_Backend/backend-moviles/api/models/Empresa.js
@@ -0,0 +1,20 @@
+/**
+ * Empresa.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    // CONFIGURACION DEL PAPA
+    Empresa: { // Nombre atributo de la relacion
+      collection: 'usuario', // Nombre del modelo a relacionar
+      via: 'fkEmpresa'  // Nombre atributo FK del otro modelo
+    },
+  }
+
+
+};
+
